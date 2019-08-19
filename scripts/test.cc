79c19d3ae2394955ec1ddce8382b7f9346c2edd5
Latypov Rustam <rustam.latypov@vaisala.com>
2019-08-19 15:31:37 +0300

    -

--- scripts/test.cc
+++ scripts/test.cc
@@ -88,9 +88,9 @@ static void run_test(int ny, int nm, int nx) {
 int main(int argc, const char** argv) {
 
     if(argc == 1) {
-        std::vector<int> nxs = {10,50,100,200,500,1000};
-        std::vector<int> nms = {5,30,60,80,100,150};
-        std::vector<int> nys = {2,5,10,100,200};
+        std::vector<int> nxs = {15,75,150,300,450,1500};
+        std::vector<int> nms = {10,50,100,200,300,1000};
+        std::vector<int> nys = {5,25,50,100,150,500};
         for(int ny : nys) {
             for(int nm : nms) {
                 for(int nx : nxs) {
