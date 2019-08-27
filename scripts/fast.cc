91fdfec6b916cc51792b9de3ff3699193104e70d
Latypov Rustam <rustam.latypov@vaisala.com>
2019-08-27 16:48:22 +0300

    -

--- scripts/fast.cc
+++ scripts/fast.cc
@@ -25,11 +25,12 @@ double4_t* pad1(int nyv, int ny, int nx, const double* data_, int P) {
     double4_t* data = double4_alloc(nyv*nx);
 
     #pragma omp parallel for
-    for (int j = 0; j < nyv; j++) {
+    for (int j = 0; j < nyv; j=j+2) {
         for (int i = 0; i < nx; i++) {
             for (int k = 0; k < P; k++) {
 
-                data[j*nx+i][k] = j*P+k < ny ? data_[(j*P+k)*nx+i] : 0;
+                data[j*nx+2*i][k] = j*P+k < ny ? data_[(j*P+k)*nx+i] : 0;
+                data[j*nx+2*i+1][k] = (j+1)*P+k < ny ? data_[(j*P+k+P)*nx+i] : 0;
 
             }
         }
@@ -37,19 +38,20 @@ double4_t* pad1(int nyv, int ny, int nx, const double* data_, int P) {
 
     return data;
 }
-
-
+    
+    
 double4_t* pad2(int nyv, int nx, int ny, const double* data_, int P) {
 
 
     double4_t* data = double4_alloc(nyv*nx);
 
     #pragma omp parallel for
-    for (int j = 0; j < nyv; j++) {
+    for (int j = 0; j < nyv; j=j+2) {
         for (int i = 0; i < nx; i++) {
             for (int k = 0; k < P; k++) {
 
-                data[j*nx+i][k] = j*P+k < ny ? data_[i*ny+(j*P+k)] : 0;
+                data[j*nx+2*i][k] = j*P+k < ny ? data_[i*ny+(j*P+k)] : 0;
+                data[j*nx+2*i+1][k] = (j+1)*P+k < ny ? data_[i*ny+(j*P+k+P)] : 0;
 
             }
         }
@@ -96,11 +98,11 @@ void fast_multiply(int ny, int nm, int nx, const double* D1_, const double* D2_,
 
             for (int k = 0; k < nx1; k++) {
                 
-                double4_t a0 = D1[(j*A)*nx1 + k];
-                double4_t a1 = D1[(j*A+1)*nx1 + k];
+                double4_t a0 = D1[(j*A)*nx1 + A*k];
+                double4_t a1 = D1[(j*A)*nx1 + A*k + 1];
 
-                double4_t b0 = D2[(i*A)*nx1 + k];
-                double4_t b1 = D2[(i*A+1)*nx1 + k];
+                double4_t b0 = D2[(i*A)*nx1 + A*k];
+                double4_t b1 = D2[(i*A)*nx1 + A*k + 1];
 
                 
                 block[0] += a0[0]*b0;
