67b2d0a3176fba37a9c157ceb1d1f8cf1ea0ba1b
Rustam Latypov <rlatypov1337@gmail.com>
2019-08-28 20:29:58 +0300

    -

--- scripts/fast.cc
+++ scripts/fast.cc
@@ -66,7 +66,7 @@ void fast_multiply(int ny, int nm, int nx, const double* D1_, const double* D2_,
     //std::vector<double> D1(nye1*nm);
     //std::memcpy(D1.data(), D1_, ny*nm*sizeof(double));
 
-    D1 = D1_;
+    double* D1 = D1_;
     
     double4_t* D2 = pad2(nyv2, ny2, nx2, D2_, P);
     ny2 = nx2;
