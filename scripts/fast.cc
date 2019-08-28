2a37126e61098ac0b5d5beae2f9469603bad1a62
Latypov Rustam <rustam.latypov@vaisala.com>
2019-08-28 16:03:06 +0300

    -

--- scripts/fast.cc
+++ scripts/fast.cc
@@ -114,7 +114,6 @@ void fast_multiply(int ny, int nm, int nx, const double* D1_, const double* D2_,
                 double4_t b0 = D2[(i*B)*nx1 + k];
                 double4_t b1 = D2[(i*B+1)*nx1 + k];
 
-                
                 block[0] += a0[0]*b0;
                 block[1] += a0[0]*b1;
 
