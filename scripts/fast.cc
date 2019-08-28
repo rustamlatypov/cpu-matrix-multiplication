6859a07fa37544f50c378e558e154e60f3f4891f
Rustam Latypov <rlatypov1337@gmail.com>
2019-08-28 19:53:19 +0300

    -

--- scripts/fast.cc
+++ scripts/fast.cc
@@ -88,6 +88,7 @@ void fast_multiply(int ny, int nm, int nx, const double* D1_, const double* D2_,
                 double a12 = D1[((j*A+1)*P+2)*nx1 + k];
                 double a13 = D1[((j*A+1)*P+3)*nx1 + k];
 
+                __builtin_prefetch(&D2[(i*B)*nx1 + k + 10]);
                 double4_t b0 = D2[(i*B)*nx1 + k];
                 double4_t b1 = D2[(i*B+1)*nx1 + k];
 
