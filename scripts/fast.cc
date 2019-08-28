819ea9d7ee101e5798701dc08335524b875f04a6
Rustam Latypov <rlatypov1337@gmail.com>
2019-08-28 20:03:08 +0300

    -

--- scripts/fast.cc
+++ scripts/fast.cc
@@ -65,7 +65,7 @@ void fast_multiply(int ny, int nm, int nx, const double* D1_, const double* D2_,
     std::vector<double> D1(nye1*nm);
     std::memcpy(D1.data(), D1_, ny*nm*sizeof(double));
     
-    double4_t* D2 = pad2(nyv2, ny2, nx2, D2_, P);
+    //double4_t* D2 = pad2(nyv2, ny2, nx2, D2_, P);
     ny2 = nx2;
 
     #pragma omp parallel for
