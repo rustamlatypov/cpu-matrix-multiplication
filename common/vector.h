ad55f3d7db5231dd39ec562de3b8799423720b19
Latypov Rustam <rustam.latypov@vaisala.com>
2019-08-28 10:48:11 +0300

    -

--- common/vector.h
+++ common/vector.h
@@ -1,14 +1,12 @@
-#ifndef VECTOR_H
-#define VECTOR_H
+#ifndef VECTOR
+#define VECTOR
 
 #include <cstdlib>
 
 typedef double double4_t __attribute__ ((vector_size (32)));
-
 constexpr double4_t double4_0 = {0,0,0,0};
 
-// Basic malloc replacement, retuns memory aligned to 32 bytes.
-// Free with free()
+// Malloc replacement, retuns memory aligned to 32 bytes, free with free()
 inline void* aligned_malloc(std::size_t bytes) {
     void* ret = nullptr;
     if (posix_memalign(&ret, 32, bytes)) {
@@ -17,7 +15,6 @@ inline void* aligned_malloc(std::size_t bytes) {
     return ret;
 }
 
-// Typed allocators
 inline double4_t* double4_alloc(std::size_t n) {
     return static_cast<double4_t*>(aligned_malloc(sizeof(double4_t) * n));
 }
