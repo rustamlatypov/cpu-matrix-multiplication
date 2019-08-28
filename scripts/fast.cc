740e395df51119346674a8a19b01d55974318575
Latypov Rustam <rustam.latypov@vaisala.com>
2019-08-28 15:37:22 +0300

    -

--- scripts/fast.cc
+++ scripts/fast.cc
@@ -81,7 +81,8 @@ void fast_multiply(int ny, int nm, int nx, const double* D1_, const double* D2_,
     //print(nm,ny,test.data());
 
     constexpr int P = 4;
-    constexpr int A = 2;
+    constexpr int A = 1;
+    constexpr int B = 1;
 
     int nye1 = ny1;
     while (nye1%(P*A) != 0) nye1++;
@@ -108,17 +109,15 @@ void fast_multiply(int ny, int nm, int nx, const double* D1_, const double* D2_,
             for (int k = 0; k < nx1; k++) {
                
             	double4_t a0 = D1[(j*A)*nx1 + k];
-                double4_t a1 = D1[(j*A+1)*nx1 + k];
-
                 double4_t b0 = D2[(i*A)*nx1 + k];
-                double4_t b1 = D2[(i*A+1)*nx1 + k];
                 
                 block[0] += a0[0]*b0;
-                block[1] += a0[0]*b1;
+                block[1] += a0[1]b0;
 
-                block[2] += a0[1]*b0;
-                block[3] += a0[1]*b1;
+                block[2] += a0[2]*b0;
+                block[3] += a0[3]*b0;
 
+                /*
                 block[4] += a0[2]*b0;
                 block[5] += a0[2]*b1;
 
@@ -136,7 +135,7 @@ void fast_multiply(int ny, int nm, int nx, const double* D1_, const double* D2_,
                 block[13] += a1[2]*b1;
 
                 block[14] += a1[3]*b0;
-                block[15] += a1[3]*b1;
+                block[15] += a1[3]*b1;*/
 
                 /*
                 //double4_t a0 = D1[(j*A)*nx1 + k];
