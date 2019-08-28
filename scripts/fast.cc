113ebcea46a375d7ee0336b674f78b771543237c
Latypov Rustam <rustam.latypov@vaisala.com>
2019-08-28 15:46:45 +0300

    -

--- scripts/fast.cc
+++ scripts/fast.cc
@@ -82,7 +82,7 @@ void fast_multiply(int ny, int nm, int nx, const double* D1_, const double* D2_,
 
     constexpr int P = 4;
     constexpr int A = 2;
-    constexpr int B = 2;
+    constexpr int B = 3;
 
     int nye1 = ny1;
     while (nye1%(P*A) != 0) nye1++;
@@ -113,32 +113,41 @@ void fast_multiply(int ny, int nm, int nx, const double* D1_, const double* D2_,
 
                 double4_t b0 = D2[(i*B)*nx1 + k];
                 double4_t b1 = D2[(i*B+1)*nx1 + k];
+                double4_t b1 = D2[(i*B+2)*nx1 + k];
                 
                 
                 block[0] += a0[0]*b0;
                 block[1] += a0[0]*b1;
+                block[2] += a0[0]*b2;
 
-                block[2] += a0[1]*b0;
-                block[3] += a0[1]*b1;
+                block[3] += a0[1]*b0;
+                block[4] += a0[1]*b1;
+                block[5] += a0[1]*b2;
 
-                block[4] += a0[2]*b0;
-                block[5] += a0[2]*b1;
+                block[6] += a0[2]*b0;
+                block[7] += a0[2]*b1;
+                block[8] += a0[2]*b2;
 
-                block[6] += a0[3]*b0;
-                block[7] += a0[3]*b1;
+                block[9] += a0[3]*b0;
+                block[10] += a0[3]*b1;
+                block[11] += a0[3]*b2;
 
 
-                block[8] += a1[0]*b0;
-                block[9] += a1[0]*b1;
+                block[12] += a1[0]*b0;
+                block[13] += a1[0]*b1;
+                block[14] += a1[0]*b2;
 
-                block[10] += a1[1]*b0;
-                block[11] += a1[1]*b1;
+                block[15] += a1[1]*b0;
+                block[16] += a1[1]*b1;
+                block[17] += a1[1]*b2;
 
-                block[12] += a1[2]*b0;
-                block[13] += a1[2]*b1;
+                block[18] += a1[2]*b0;
+                block[19] += a1[2]*b1;
+                block[20] += a1[2]*b2;
 
-                block[14] += a1[3]*b0;
-                block[15] += a1[3]*b1;
+                block[21] += a1[3]*b0;
+                block[22] += a1[3]*b1;
+                block[23] += a1[3]*b2;
 
                 /*
                 //double4_t a0 = D1[(j*A)*nx1 + k];
