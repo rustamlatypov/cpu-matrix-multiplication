dcaa20f346d18c57f2db636f8eeaf36d4adfc5a6
Latypov Rustam <rustam.latypov@vaisala.com>
2019-08-28 13:47:04 +0300

    -

--- scripts/fast.cc
+++ scripts/fast.cc
@@ -7,7 +7,7 @@
 #include "vector.h"
 #include "helper.h"
 
-// Vertical padding such that rows are devisible by P*S
+// vertical padding such that rows are devisible by P*A
 // 1  2  3  4  5     1  2  3  4  5
 // 6  7  8  9  10    6  7  8  9  10
 // 11 12 13 14 15 => 11 12 13 14 15
@@ -17,9 +17,7 @@
 //                   0  0  0  0  0
 //                   0  0  0  0  0
 
-// When A*B, pad1 is for A and pad2 is for B
-// pad1 is straighforward vertical padding
-// pad2 performes a transpose as well as vertical padding
+// when A*B, pad1 is for A and pad2 is for B
 
 
 double4_t* pad1(int nyv, int ny, int nx, const double* data_, int P) {
@@ -69,39 +67,42 @@ void fast_multiply(int ny, int nm, int nx, const double* D1_, const double* D2_,
     int nx2 = nx;
 
     constexpr int P = 4;
-    constexpr int S = 2;
+    constexpr int A = 2;
 
     int nye1 = ny1;
-    while (nye1%(P*3) != 0) nye1++;
+    while (nye1%(P*A) != 0) nye1++;
     int nyv1 = nye1/P;
-    int nyb1 = nyv1/3;
+    int nyb1 = nyv1/A;
 
     int nye2 = nx2;
-    while (nye2%(P*S) != 0) nye2++;
+    while (nye2%(P*A) != 0) nye2++;
     int nyv2 = nye2/P;
-    int nyb2 = nyv2/S;
+    int nyb2 = nyv2/A;
 
     double4_t* D1 = pad1(nyv1, ny1, nx1, D1_, P);
     double4_t* D2 = pad2(nyv2, ny2, nx2, D2_, P);
 
     ny2 = nx2;
 
+    //print_v(D1, nyv1, nx1, P);
+    //print_v(D2, nyv2, nx2, P);
+
     #pragma omp parallel for
     for (int j = 0; j < nyb1; j++) {
 
         for (int i = 0; i < nyb2; i++) {
 
-            double4_t block[S*3*P] = {double4_0};
+            double4_t block[A*A*P] = {double4_0};
 
             for (int k = 0; k < nx1; k++) {
                 
-                double4_t a0 = D1[(j*3)*nx1 + k];
-                double4_t a1 = D1[(j*3+1)*nx1 + k];
-                double4_t a2 = D1[(j*3+2)*nx1 + k];
+                double4_t a0 = D1[(j*A)*nx1 + k];
+                double4_t a1 = D1[(j*A+1)*nx1 + k];
 
-                double4_t b0 = D2[(i*S)*nx1 + k];
-                double4_t b1 = D2[(i*S+1)*nx1 + k];
-        
+                double4_t b0 = D2[(i*A)*nx1 + k];
+                double4_t b1 = D2[(i*A+1)*nx1 + k];
+
+                
                 block[0] += a0[0]*b0;
                 block[1] += a0[0]*b1;
 
@@ -126,43 +127,25 @@ void fast_multiply(int ny, int nm, int nx, const double* D1_, const double* D2_,
 
                 block[14] += a1[3]*b0;
                 block[15] += a1[3]*b1;
-
-
-                block[16] += a2[0]*b0;
-                block[17] += a2[0]*b1;
-
-                block[18] += a2[1]*b0;
-                block[19] += a2[1]*b1;
-
-                block[20] += a2[2]*b0;
-                block[21] += a2[2]*b1;
-
-                block[22] += a2[3]*b0;
-                block[23] += a2[3]*b1;
             }
-            /*
-            for (int jj1 = 0; jj1 < P*3; jj1++) {
-                for (int jj2 = 0; jj2 < S; jj2++) {
 
+            for (int jj1 = 0; jj1 < P*A; jj1++) {
+                for (int jj2 = 0; jj2 < A; jj2++) {
                     for (int ii = 0; ii < P; ii++) {
 
-                        int jjj = j*3 * P + jj1;
-                        int iii = i*S * P + jj2*P + ii;
+                        int jjj = j*A * P + jj1;
+                        int iii = i*A * P + jj2*P + ii;
 
                         if (jjj < ny1 && iii < ny2) {
-                            result[jjj * ny2 + iii] = block[jj1*3+jj2][ii];
+                            result[jjj * ny2 + iii] = block[jj1*A+jj2][ii];
                         }
 
                     }
-
                 }
-            }*/
+            }
         }
     }
     free(D1);
     free(D2);
-
-    //print(ny, nm, D1_);
-    //print(nm, nx, D2_);
-    //print(ny, nx, result);
+    //print(ny1, nx2, result);
 }
