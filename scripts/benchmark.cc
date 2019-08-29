37bf5876e1713b81204593d7be02cf9d530e5c1b
Latypov Rustam <rustam.latypov@vaisala.com>
2019-08-29 15:15:27 +0300

    -

--- scripts/benchmark.cc
+++ scripts/benchmark.cc
@@ -24,7 +24,8 @@ static void benchmark(int dim, int iter) {
         printf("%.3f\t", t);
         times.push_back(t);
 
-        double cpu_usage = 2*dim/1000*dim/1000*dim/1000/t/230;
+
+        double cpu_usage = 2*dim*dim/1000000*dim/1000/t/230;
         printf("%.3f\n", cpu_usage);
         cpus.push_back(cpu_usage);
     }
