bef29fc9079345e9c1deac4095917365c2ccb468
Latypov Rustam <rustam.latypov@vaisala.com>
2019-08-30 10:49:29 +0300

    -

--- scripts/help.cc
+++ scripts/help.cc
@@ -4,8 +4,8 @@ int main(int argc, const char** argv) {
     
     if (argc==1) {
 
-    	std::string s4 = "./help <br/> Expains the commands in short.";
+    	string s4 = "./help <br/> Expains the commands in short.";
 
-		printf("%s\n", s4);
+    	std::cout << s4 << std::endl;
     }
 }
