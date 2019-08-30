5124e43d4c7a8e937a8053587bf1ec34a0cebb6e
Latypov Rustam <rustam.latypov@vaisala.com>
2019-08-30 10:46:32 +0300

    -

--- scripts/help.cc
+++ scripts/help.cc
@@ -3,7 +3,7 @@
 int main(int argc, const char** argv) {
     
     if (argc==1) {
-    	std::string s1 = "`./run <dim> <iter>` default: `dim=1000`, `iter=3` <br/>
+    	std::string s1 = "./run <dim> <iter> default: `dim=1000`, `iter=3` 
 Runs both implementations on the same matrices for `iter` times and outputs the average running times, cpu usages, the speedup obtained and the error term.";
     	std::string s2 = "`./test <ny> <nm> <nx> <verbose>` default: `ny,nm,nx in {5,1500}`, `verbose=0` <br/>
 Runs both implementations on different matrices and different combinations of `ny`, `nm` and `nx`. Dimension wise the multiplication is `(ny*nm)(nm*nx)`. Each combination is run **10** times and the error term is accumulated and outputed. Each test passes if the accumulated error is below **1e-3**. Output can be enabled using `verbose=1`.";
