c422efd61ae25f2e7a342a330f5174e90c85e454
Latypov Rustam <rustam.latypov@vaisala.com>
2019-08-30 11:04:12 +0300

    -

--- scripts/help.cc
+++ scripts/help.cc
@@ -10,8 +10,7 @@ int main(int argc, const char** argv) {
     	std::string s1 = "\n";
     	std::string s2 = "\n";
     	std::string s3 = "\n";
-    	std::string s4 = "   -`./help`\n   Expains the commands in short.";
-    	std::replace(s4.begin(), s4.end(), '`', ''');
+    	std::string s4 = "   -'./help'\n   Expains the commands in short.";
 
     	std::cout << s1 << std::endl;
     	std::cout << s2 << std::endl;
