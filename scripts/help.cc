b62ce192cd48366a065d7f78ed0a08bd1cbce411
Latypov Rustam <rustam.latypov@vaisala.com>
2019-08-30 10:53:00 +0300

    -

--- scripts/help.cc
+++ scripts/help.cc
@@ -4,7 +4,9 @@ int main(int argc, const char** argv) {
     
     if (argc==1) {
 
-    	std::string s3 = "'./benchmark <dim> <iter>' default: 'dim=3000', 'iter=10'\nRuns the parallel implementation on the same square matrices of dimension 'dim' for `iter` times and outputs running times, cpu usages and the their averages.";
+    	std::string s3 = "'./benchmark <dim> <iter>' default: 'dim=3000', 'iter=10'\nRuns the parallel implementation 
+    	on the same square matrices of dimension 'dim' for `iter` times and outputs running times, cpu usages and the 
+    	their averages.";
     	std::string s4 = "'./help'\nExpains the commands in short.";
 
     	std::cout << s3 << std::endl;
