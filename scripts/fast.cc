0f262e2bdb8d483415aa0e30494fb28115825a68
Rustam Latypov <rlatypov1337@gmail.com>
2019-09-24 17:29:44 +0300

    tiling implemented

--- scripts/fast.cc
+++ scripts/fast.cc
@@ -97,7 +97,7 @@ void fast_multiply(int ny, int nm, int nx, const double* D1_, const double* D2_,
                     // performace critical loop
                     for (int k = 0; k < nx1; k++) {
 
-                        /*
+                        
                         double a00 = D1[(j*A*P+0)*nx1 + k];
                         double a01 = D1[(j*A*P+1)*nx1 + k];
                         double a02 = D1[(j*A*P+2)*nx1 + k];
@@ -110,9 +110,9 @@ void fast_multiply(int ny, int nm, int nx, const double* D1_, const double* D2_,
 
                         double4_t b0 = D2[(i*B)*nx1 + k];
                         double4_t b1 = D2[(i*B+1)*nx1 + k];
-                        */
 
-                        //to be able to test cpu without memory bottleneck
+                        /*
+                        to be able to test cpu without memory bottleneck
 
                         volatile double a00 = D1[0];
                         volatile double a01 = D1[0];
@@ -126,7 +126,7 @@ void fast_multiply(int ny, int nm, int nx, const double* D1_, const double* D2_,
 
                         volatile double4_t b0 = D2[0];
                         volatile double4_t b1 = D2[0];
-                        
+                        */
 
                         block[0] += a00*b0;
                         block[1] += a00*b1;
