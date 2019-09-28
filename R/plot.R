79abcb8506102d08d990db552f3c29ecf609b47b
Rustam Latypov <rlatypov1337@gmail.com>
2019-09-28 17:17:40 +0300

    -

--- R/plot.R
+++ R/plot.R
@@ -6,7 +6,7 @@ data <- data.frame(n, base, para)
 
 par(mar = c(5,5,5,5))
 plot(data$n, data$base, type="b", xlab = "n", ylab = "seconds", bty = "n", log = "y",
-     lwd = 1.6, cex = 1.2, cex.axis = 1.3, cex.lab = 1.3, ylim=c(0.1,6000))
+     lwd = 1.6, cex = 1.2, cex.axis = 1.3, cex.lab = 1.3, ylim=c(0.1,3000))
 lines(data$n, data$para, type="b", lwd = 1.6, cex = 1.2)
 
 
